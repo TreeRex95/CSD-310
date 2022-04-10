ff86daf3a3cdffa6ad4f4f77a3ba611b54db6219
TreeRex95 <97421903+TreeRex95@users.noreply.github.com>
2022-04-10 16:44:51 -0500

    Update mongodb_test.py

--- module_5/mongodb_test.py
+++ module_5/mongodb_test.py
@@ -1,1 +1,26 @@
+"""
+    Title: mongodb_test.py
+    Author: Tristan Boetcher
+    Date 4/10/22
+    Description: Test program for connecting to a 
+                 MongoDB Atlas Cluster
+"""
 
+""" import statements """
+from pymongo import MongoClient
+
+# MongoDB connection string
+url = "mongodb+srv://admin:admin@cluster0.qxtd6.mongodb.net/pytech"
+
+# connect to the MongoDB cluster
+client = MongoClient(url)
+
+# connect pytech database
+db = client.pytech
+
+# show the connected collections
+print("\n -- Pytech COllection List --")
+print(db.list_collection_names())
+
+# show an exit message
+input("\n\n  End of program, press any key to exit... ")
