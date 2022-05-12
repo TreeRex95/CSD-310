f7c97f4a1233d4c2b8adee697cc549676987231d
TreeRex95 <97421903+TreeRex95@users.noreply.github.com>
2022-05-12 10:18:49 -0500

    Update whatabook.init.sql

--- module_10/whatabook.init.sql
+++ module_10/whatabook.init.sql
@@ -5,6 +5,25 @@
     Description: WhatABook database initialization script.
 */
 
+-- drop test user if exists 
+DROP USER IF EXISTS 'whatabook_user'@'localhost';
+
+-- create whatabook_user and grant them all privileges to the whatabook database 
+CREATE USER 'whatabook_user'@'localhost' IDENTIFIED WITH mysql_native_password BY 'MySQL8IsGreat!';
+
+-- grant all privileges to the whatabook database to user whatabook_user on localhost 
+GRANT ALL PRIVILEGES ON whatabook.* TO'whatabook_user'@'localhost';
+
+-- drop contstraints if they exist
+ALTER TABLE wishlist DROP FOREIGN KEY fk_book;
+ALTER TABLE wishlist DROP FOREIGN KEY fk_user;
+
+-- drop tables if they exist
+DROP TABLE IF EXISTS store;
+DROP TABLE IF EXISTS book;
+DROP TABLE IF EXISTS wishlist;
+DROP TABLE IF EXISTS user;
+
 /*
     Create table(s)
 */
